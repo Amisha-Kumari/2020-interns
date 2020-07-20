728489bc129b78e9ef2becfcaa9b9b4690ac2a98
Amisha-Kumari <68562736+Amisha-Kumari@users.noreply.github.com>
2020-07-21 03:20:27 +0530

    Update Task3.py

--- Task3.py
+++ Task3.py
@@ -8,7 +8,6 @@ Created on Mon Jul 20 18:07:23 2020
 
 import json
 import matplotlib.pyplot as plt 
-import collections
 from datetime import datetime as d
 import numpy as np
 
@@ -21,8 +20,7 @@ with open('latest-rates.json') as f:
 first=d(2019,1,1)
 last=d(2019,1,31)
 
-values = data["rates"]
-values =collections.OrderedDict(sorted(values.items()))
+values = sorted(data["rates"])
 
 valueINR=[]
 valueGBP=[]
